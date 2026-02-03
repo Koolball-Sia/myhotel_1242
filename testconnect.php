bd38bfdcb27f9e05e931ab867ca0d3e3f86f3494
BaluNamu <koolball.sia@gmail.com>
2026-02-03 15:31:23 +0700

    final1

---
+++ testconnect.php
@@ -0,0 +1,11 @@
+<?php
+$objCon = mysqli_connect("localhost", "root", "", "");
+
+// Check connection
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+} else {
+    echo "success";
+}
+?>
